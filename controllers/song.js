ed58cdfcb73ff7c4a17473efe16f35b5ce29df57
uiyoung <suy2suy@gmail.com>
2023-05-22 18:42:50 +0900

    replace -> regex

--- controllers/song.js
+++ controllers/song.js
@@ -1,13 +1,23 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+function getSpaceIgnoreRegex(str) {
+  return str === '%'
+    ? '.*'
+    : str
+        .split('')
+        .filter((e) => e !== ' ')
+        .join('[[:space:]]?');
+}
+
 exports.getTotalCount = async (req, res, next) => {
   const { title } = req.query;
+  const titleRegex = getSpaceIgnoreRegex(title);
   try {
     const result = await prisma.$queryRaw`
       SELECT COUNT(*)::integer as count 
       FROM jeil."Song"
-      WHERE LOWER(REPLACE(title, ' ', '')) like '%' || LOWER(REPLACE(${title}, ' ', '')) || '%'
+      WHERE title ~* ${titleRegex}
     `;
 
     res.json(result[0]);
@@ -19,22 +29,39 @@ exports.getTotalCount = async (req, res, next) => {
 exports.getSongs = async (req, res, next) => {
   const SONGS_PER_PAGE = 10;
   const { title, page } = req.query;
+  const titleRegex = getSpaceIgnoreRegex(title);
   try {
-    // const songs = await prisma.song.findMany({
-    //   select: { id: true, title: true, lyrics: true, type: true },
-    //   skip: 0 * Number(page),
-    //   take: 10,
-    //   orderBy: [{ id: 'asc' }],
-    // });
-
+    // 공백포함 검색을 위한 raw query 사용
     const songs = await prisma.$queryRaw`
       SELECT id, title, type
       FROM jeil."Song"
-      WHERE LOWER(REPLACE(title, ' ', '')) like '%' || LOWER(REPLACE(${title}, ' ', '')) || '%'
+      WHERE title ~* ${titleRegex}
       ORDER BY id DESC
       LIMIT ${SONGS_PER_PAGE} 
       OFFSET ${(page - 1) * SONGS_PER_PAGE}
     `;
+    // const songs = await prisma.$queryRaw`
+    //   SELECT id, title, type
+    //   FROM jeil."Song"
+    //   WHERE REPLACE(title, ' ', '') ILIKE '%' || REPLACE(${title}, ' ', '') || '%'
+    //   ORDER BY id DESC
+    //   LIMIT ${SONGS_PER_PAGE}
+    //   OFFSET ${(page - 1) * SONGS_PER_PAGE}
+    // `;
+
+    // const songs = await prisma.song.findMany({
+    //   select: { id: true, title: true, lyrics: true, type: true },
+    //   where: {
+    //     title: {
+    //       contains: `${title}`,
+    //       mode: 'insensitive',
+    //     },
+    //   },
+    //   skip: (Number(page) - 1) * SONGS_PER_PAGE,
+    //   take: SONGS_PER_PAGE,
+    //   orderBy: [{ id: 'desc' }],
+    // });
+
     res.json(songs);
   } catch (error) {
     console.error(error);
