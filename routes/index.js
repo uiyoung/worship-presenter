42bde5e4a591aaf9c40d0c0aea247b0e7ac67f31
uiyoung <suy2suy@gmail.com>
2023-06-03 03:55:45 +0900

    add about page

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,7 @@ router.get('/', (req, res) => {
   res.render('song-list', { title: 'Worship Presenter' });
 });
 
+router.get('/about', (req, res) => {
+  res.render('about', { title: 'about - Worship Presenter' });
+});
 module.exports = router;
