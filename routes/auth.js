be1c3850c7af5a2a0186113baada4da1079d90ae
uiyoung <suy2suy@gmail.com>
2023-06-04 04:12:55 +0900

    comment signup router

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const { isLoggedIn, isNotLoggedIn } = require('../middlewares');
 
 router.route('/login').get(isNotLoggedIn, renderLogin).post(isNotLoggedIn, login);
 router.post('/logout', isLoggedIn, logout);
-router.route('/signup').get(isNotLoggedIn, renderSignup).post(isNotLoggedIn, signup);
+//router.route('/signup').get(isNotLoggedIn, renderSignup).post(isNotLoggedIn, signup);
 router.route('/:id').get().patch().delete();
 
 module.exports = router;
