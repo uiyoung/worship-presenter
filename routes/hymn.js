6c62a18d2ac79f5619b638ca55ef236613a27769
uiyoung <suy2suy@gmail.com>
2023-08-11 20:31:30 +0900

    add update hymn lyrics

--- routes/hymn.js
+++ routes/hymn.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const { getHymnImagesById } = require('../controllers/hymn');
+const { getHymnImagesById, updateLyrics } = require('../controllers/hymn');
+const { isLoggedIn } = require('../middlewares/');
 
 router.get('/images/:no', getHymnImagesById);
+router.patch('/lyrics/:no', isLoggedIn, updateLyrics);
 
 module.exports = router;
